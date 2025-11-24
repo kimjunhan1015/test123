bad402e28491a1b05a9aceb396bc94d24db6c704
junha <you@example.com>
2025-11-24 14:15:36 +0900

    Add Firebase configuration

--- firebase-config.js
+++ firebase-config.js
@@ -2,11 +2,11 @@
 // Firebase Console (https://console.firebase.google.com/)에서 프로젝트 설정 정보를 가져와서 아래에 입력하세요
 
 export const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT_ID.appspot.com",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyDV-9nbMhFtPxcWX1vQKG9303dSszS7Zfc",
+  authDomain: "jhmalgntest.firebaseapp.com",
+  projectId: "jhmalgntest",
+  storageBucket: "jhmalgntest.firebasestorage.app",
+  messagingSenderId: "797758780412",
+  appId: "1:797758780412:web:a05d4f004e3c0a6ad67879"
 };
 
